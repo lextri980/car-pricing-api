b1986ea574455bde973a63945f81e8fd9e828536
trilx <trilx@runsystem.net>
2024-04-11 14:48:31 +0700

    update

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -8,7 +8,11 @@ import { plainToClass } from 'class-transformer';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-export function Serialize(dto: any) {
+interface ClassConstructor {
+  new (...args: any[]): object;
+}
+
+export function Serialize(dto: ClassConstructor) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 
@@ -16,16 +20,8 @@ export class SerializeInterceptor implements NestInterceptor {
   constructor(private dto: any) {}
 
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
-    console.log('context', context);
-
     return handler.handle().pipe(
       map((data: any) => {
-        console.log(
-          'ashdashduahdhdoaidhio',
-          plainToClass(this.dto, data, {
-            excludeExtraneousValues: true,
-          }),
-        );
         // console.log('Run after return data', data);
         return plainToClass(this.dto, data, {
           excludeExtraneousValues: true,
